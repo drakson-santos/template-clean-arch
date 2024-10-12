59ea05aec8e4331f88c3984e175430f8dd0453ee
Darkson <darkson@MacBook-Air-de-Darkson.local>
2024-10-12 01:36:02 -0300

    fix type

--- src/data/services/user/user.http.service.ts
+++ src/data/services/user/user.http.service.ts
@@ -1,7 +1,8 @@
 import type { IUserService } from "@/domain/services/user/user.services";
+import type { IUserLogin } from "@/domain/useCases/userLogin/user-login.use-case";
 
 export class UserService implements IUserService {
-	async loginBy(email: string, password: string): Promise<void> {
+	async loginBy(email: string, password: string): Promise<IUserLogin.Response> {
 		if (!email) throw new Error("Email is required.");
 		if (!password) throw new Error("Password is required.");
 		// use http client
