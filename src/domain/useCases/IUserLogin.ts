d9ea0f59ffc96b9d187d8f4d5fe17003646d3520
Darkson <darkson@MacBook-Air-de-Darkson.local>
2024-10-11 22:57:20 -0300

    add user login use case

---
+++ src/domain/useCases/IUserLogin.ts
@@ -0,0 +1,10 @@
+export interface IUserLogin {
+    loginBy: (params: IUserLogin.Params) => Promise<void>
+}
+
+namespace IUserLogin {
+    export type Params = {
+        email: string
+        password: string
+    }
+}
