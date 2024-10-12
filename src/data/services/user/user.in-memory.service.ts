400c030650eba7a93252085895cd80c7ca2e8e22
Darkson <darkson@MacBook-Air-de-Darkson.local>
2024-10-12 02:01:49 -0300

    add validate email and password

--- src/data/services/user/user.in-memory.service.ts
+++ src/data/services/user/user.in-memory.service.ts
@@ -3,9 +3,6 @@ import type { IUserLogin } from "@/domain/useCases/userLogin/user-login.use-case
 
 export class UserInMemoryService implements IUserService {
 	async loginBy(email: string, password: string): Promise<IUserLogin.Response> {
-		if (!email) throw new Error("Email is required.");
-		if (!password) throw new Error("Password is required.");
-
 		const isCorrectEmail = email === "xuxa@gmail.com";
 		const isCorrectPassword = password === "Abcd@123";
 		const isUnauthorized = !isCorrectEmail || !isCorrectPassword;
--- src/data/useCases/userLogin/user-login.use-case.ts
+++ src/data/useCases/userLogin/user-login.use-case.ts
@@ -4,7 +4,32 @@ import type { IUserLogin } from "@/domain/useCases/userLogin/user-login.use-case
 export class UserLoginUseCase {
 	constructor(private readonly userService: IUserService) {}
 
-	login(user: IUserLogin.User): Promise<IUserLogin.Response> {
+	private validateEmail(email: string): void {
+		if (!email) throw new Error("Email is required.");
+
+		const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		if (!emailPattern.test(email)) throw new Error("Invalid email format.");
+	}
+
+	private validatePassword(password: string): void {
+		if (!password) throw new Error("Password is required.");
+
+		const strongPasswordPattern =
+			/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+		if (!strongPasswordPattern.test(password)) {
+			throw new Error(
+				"Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character.",
+			);
+		}
+	}
+
+	private validateUser(user: IUserLogin.User) {
+		this.validateEmail(user.email);
+		this.validatePassword(user.password);
+	}
+
+	async login(user: IUserLogin.User): Promise<IUserLogin.Response> {
+		this.validateUser(user);
 		return this.userService.loginBy(user.email, user.password);
 	}
 }
